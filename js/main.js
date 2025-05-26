d2f8735ecd23b7bb8122091a2c31fb06c6666f3c
darshanht26 <darshanht26@users.noreply.github.com>
2025-05-26 10:31:16 +0000

    deploy: 41da04b2a6526f1f7308b31bf9ea49ed09f2525c

---
+++ js/main.js
@@ -0,0 +1,63 @@
+
+
+// sticky header
+const siteHeader = document.querySelector(".site-header");
+function stickyOnScroll() {
+  if (window.scrollY >= siteHeader.offsetHeight) {
+    siteHeader.classList.add("sticky-header");
+    document.body.style.paddingTop = `${siteHeader.offsetHeight}px`;
+  } else {
+    siteHeader.classList.remove("sticky-header");
+    document.body.style.paddingTop = null;
+  }
+}
+
+// Menu toggle
+const menuToggle = document.querySelector('#menu-toggle');
+const primaryNav = document.querySelector('#primary-nav');
+
+function menuFn() {
+  if (document.body.classList.contains('body-menu-open')) {
+    document.documentElement.classList.remove('overflow-hidden')
+    document.body.classList.remove('body-menu-open')
+    primaryNav.style.paddingTop = null;
+  } else {
+    document.documentElement.classList.add('overflow-hidden')
+    document.body.classList.add('body-menu-open')
+    primaryNav.style.paddingTop = `${siteHeader.offsetHeight}px`
+  }
+}
+
+menuToggle.addEventListener("click", menuFn);
+
+// menu nav click jump to section
+const navLinks = document.querySelectorAll('.primary-nav-list a');
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', (e) => {
+    menuFn()
+  })
+})
+
+// accordion function
+const accordionTitles = document.querySelectorAll(".team-card-header");
+accordionTitles.forEach((accordionTitle, i) => {
+  accordionTitle.addEventListener("click", () => {
+    if (accordionTitle.parentElement.parentElement.classList.contains("is-open")) {
+      accordionTitle.parentElement.parentElement.classList.remove("is-open");
+      accordionTitle.nextElementSibling.style.maxHeight = null
+    } else {
+      const accordionTitlesWithIsOpen = document.querySelectorAll(".is-open");
+      accordionTitlesWithIsOpen.forEach((accordionTitleWithIsOpen) => {
+        accordionTitleWithIsOpen.classList.remove('is-open')
+        accordionTitleWithIsOpen.querySelector('.team-card-body').style.maxHeight = null
+      });
+      accordionTitle.parentElement.parentElement.classList.add("is-open");
+      accordionTitle.nextElementSibling.style.maxHeight = `${accordionTitle.nextElementSibling.scrollHeight}px`
+    }
+  });
+});
+
+// Apply for window scroll
+window.addEventListener("scroll", function () {
+  stickyOnScroll()
+});
